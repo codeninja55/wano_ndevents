897ee8f142ab2e33761d3d3356dea519c7ec8f1c
Andrew Che <andrew@codeninja55.me>
2018-04-16 19:21:47 +1000

    Changed app name to NDEvents_api and deleted old dir

---
+++ backend_django_api/NDEvents_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NdeventsApiConfig(AppConfig):
+    name = 'NDEvents_api'
--- backend_django_api/backend_django_api/settings.py
+++ backend_django_api/backend_django_api/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'events_api',
+    'NDEvents_api',
 ]
 
 MIDDLEWARE = [
