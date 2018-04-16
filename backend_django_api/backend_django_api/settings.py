2b8048e4236003790f317e8eb455ca8a3f6f351f
Andrew Che <andrew@codeninja55.me>
2018-04-16 22:26:39 +1000

    changed database to mysql

--- backend_django_api/backend_django_api/settings.py
+++ backend_django_api/backend_django_api/settings.py
@@ -75,13 +75,26 @@ WSGI_APPLICATION = 'backend_django_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# Default Database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'databaseNO',
+        # 'USER': 'wano',
+        # 'PASSWORD': '%Co22o',
+        # 'HOST': '127.0.0.1',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'my.cnf'),
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
