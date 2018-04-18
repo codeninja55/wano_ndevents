aa5d6c71ad60b362ee288feb5942dfcf8860daa9
Andrew Che <andrew@codeninja55.me>
2018-04-18 22:24:28 +1000

    Added basic attributes to Event model

--- backend_django_api/NDEvents_api/models.py
+++ backend_django_api/NDEvents_api/models.py
@@ -1,3 +1,41 @@
+from datetime import datetime, timedelta
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+__author__ = 'codeninja55'
+
+
+def default_time_end():
+    return datetime.now() + timedelta(hours=1)
+
+
+# Events model
+class Events(models.Model):
+    event_id = models.AutoField(primary_key=True)
+    organisers_name = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField('event title', max_length=30, blank=False, null=False)
+    description = models.TextField('event description', max_length=200, blank=True)
+    venue = models.TextField('event location', max_length=200, null=False, blank=False)
+    capacity_max = models.IntegerField('max capacity', blank=True, null=False, default=0)
+    capacity_expected = models.IntegerField('expected capacity', blank=True, null=False, default=0)
+    date_start = models.DateField('event start date', auto_now=True, auto_now_add=True)
+    date_end = models.DateField('event end date', auto_now=False, auto_now_add=False)
+    time_start = models.TimeField('event start time', auto_now=False, auto_now_add=True)
+    time_end = models.TimeField('event end time', auto_now=False, default=default_time_end)
+    date_created = models.DateTimeField('date event created', auto_now=False, auto_now_add=True)
+    last_updated = models.DateTimeField('last updated date', auto_now=True, auto_now_add=False)
+    launch_date = models.DateTimeField('date event launched', auto_now_add=False, auto_now=False)
+    is_launched = models.BooleanField('event launched', default=False)
+
+    def __str__(self):
+        return '%s %s %s (%s)'.format(self.title, "@", self.venue, self.date_start)
+
+
+class Ticket(models.Model):
+    pass
+
+
+# Need to update the tickets available as these are saved.
+class Booking(models.Model):
+    pass
