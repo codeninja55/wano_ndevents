72027a2133de2806978c5b4e215f5c5e85b74840
Andrew Che <andrew@neuraldev.io>
2018-04-19 23:15:30 +1000

    Added event.service to consume an api and allow an observable

---
+++ frontend-angular/src/app/event.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable()
+export class EventService {
+
+  constructor(private http: HttpClient) {}
+
+  // Uses http.get() to load data from a single API endpoint
+  getEvents() {
+    return this.http.get('http://127.0.0.1:8000/api/event/');
+  }
+}
