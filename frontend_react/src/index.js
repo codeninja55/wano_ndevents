f682d4d2506fc2fd778b9db1c2055bdf66b1d7cd
Andrew Che <andrew@codeninja55.me>
2018-04-19 16:35:02 +1000

    Made some minor changes to all components

--- frontend_react/src/index.js
+++ frontend_react/src/index.js
@@ -7,53 +7,52 @@ import './css/bootstrap.min.css';
 //import './js/materialize.js';
 import './index.css';
 
-[
+let data = [
     {
-    "event_id":1,
-    "title": "event1",
-    "description": "this is the first event",
-    "organisers_name": 1,
-    "date_start":"2018-04-20",
-    "date_end":"2018-04-20",
-    "time_end": "04:00:00",
-    "time_start": "03:00:00",
-    "date_created":"2018-04-19"
+        "event_id":1,
+        "title": "event1",
+        "description": "this is the first event",
+        "organisers_name": 1,
+        "date_start":"2018-04-20",
+        "date_end":"2018-04-20",
+        "time_end": "04:00:00",
+        "time_start": "03:00:00",
+        "date_created":"2018-04-19"
     },
     {
-    "event_id":2,
-    "title": "event2",
-    "description": "this is the second event",
-    "organisers_name": 1,
-    "date_start":"2018-4-20",
-    "date_end":"2018-4-21",
-    "time_start": "03:00:00",
-    "time_end": "04:00:00",
-    "date_created":19-4-2018
+        "event_id":2,
+        "title": "event2",
+        "description": "this is the second event",
+        "organisers_name": 1,
+        "date_start":"2018-4-20",
+        "date_end":"2018-4-21",
+        "time_start": "03:00:00",
+        "time_end": "04:00:00",
+        "date_created":19-4-2018
     },
     {
-    "event_id":3,
-    "title": "event3",
-    "description": "this is the first event",
-    "organisers_name": 1,
-    "date_start":"2018-4-20",
-    "date_end":"2018-4-21",
-    "time_start": "03:00:00",
-    "time_end": "04:00:00",
-    "date_created":19-4-2018
+        "event_id":3,
+        "title": "event3",
+        "description": "this is the first event",
+        "organisers_name": 1,
+        "date_start":"2018-4-20",
+        "date_end":"2018-4-21",
+        "time_start": "03:00:00",
+        "time_end": "04:00:00",
+        "date_created":19-4-2018
     },
-]
-
+];
 
 
 //Changing the search bar into an image or icon might not work well
 class Header extends React.Component{
     render(){
         return(
-            <div class = "header">
-                <div class = "search-container">
+            <div className="header">
+                <div className="search-container">
                     <form action="">
-                        <input class= "searchBar" classtype="text" placeholder="Search. . ." name="search" />
-                        <button class= "searchButton" type="submit">&nbsp;&nbsp;&nbsp;</button>
+                        <input className="searchBar" type="text" placeholder="Search. . ." name="search" />
+                        <button className="searchButton" type="submit">&nbsp;&nbsp;&nbsp;</button>
                     </form>
                 </div>
                 NDev
@@ -82,34 +81,19 @@ class TabContent extends React.Component{
     }
 }
 
-class Tabs extends React.Component{
-    renderTabButton(i){
-        return <TabButton />
-    }
-
-    renderTabContent(){
-        return <TabContent />
-    }
-    render(){
-        return(
-          <div>
-          <div>
-            {this.renderTabButton("Current Events")}
-            {this.renderTabButton("Past Events")}
-          </div>
-          <div class= "tab">
-            CurrentEvent
-          </div>
-          </div>  
-        );
+class RightColumn extends React.Component {
+    render() {
+        return (
+            <aside className=""> TEST RIGHT</aside>
+        )
     }
 }
 
-class SideNav extends React.Component{
+class LeftColumn extends React.Component {
     render(){
         return(
-            <aside class = "sideNav">
-                <Tabs />
+            <aside>
+                Test LEFT
             </aside>
         );
     }
@@ -120,7 +104,8 @@ class App extends React.Component{
         return(
             <div>
                 <Header />
-                <SideNav />
+                <RightColumn />
+                <LeftColumn />
             </div>
         );
     }
