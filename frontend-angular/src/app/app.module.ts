5cd49dc940081db980ef2436d10701f5e7492f6b
Andrew Che <andrew@neuraldev.io>
2018-04-19 22:28:43 +1000

    Added all necessary material modules

--- frontend-angular/src/app/app.module.ts
+++ frontend-angular/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 
+// All Angular Material Modules imported at once
+import { MaterialModule } from './material-module';
+
+// Custom Project Components
+import {NdevNavbarComponent} from './ndev-navbar/ndev-navbar.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NdevNavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
