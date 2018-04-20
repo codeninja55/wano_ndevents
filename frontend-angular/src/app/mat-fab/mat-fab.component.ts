58e6f31740858607efa0d002d111c4d4c7e98533
Andrew Che <andrew@neuraldev.io>
2018-04-20 15:04:11 +1000

    Added a mat-fab component to the bottom of the page

---
+++ frontend-angular/src/app/mat-fab/mat-fab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mat-fab',
+  templateUrl: './mat-fab.component.html',
+  styleUrls: ['./mat-fab.component.css']
+})
+export class MatFabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
