ce61bcd246d02583a5159df2b4c8cd9131eecaf2
Andrew Che <andrew@codeninja55.me>
2018-04-23 01:31:26 +1000

    Added a DateAdapter for MomentJS to be used with MatDatePicker

--- frontend-angular/src/app/app.module.ts
+++ frontend-angular/src/app/app.module.ts
@@ -14,8 +14,10 @@ import { EventsTabComponent } from './events-tab/events-tab.component';
 import { MatFabComponent } from './mat-fab/mat-fab.component';
 import { EventDetailComponent } from './event-detail/event-detail.component';
 import { BookingsTabComponent } from './bookings-tab/bookings-tab.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { EventFormComponent } from './event-form/event-form.component';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material';
+import {MAT_MOMENT_DATE_FORMATS, MomentDateAdapter} from '@angular/material-moment-adapter';
 
 @NgModule({
   declarations: [
@@ -34,7 +36,10 @@ import { EventFormComponent } from './event-form/event-form.component';
     HttpClientModule,
     AppRoutingModule,
   ],
-  providers: [ EventService ],
+  providers: [ EventService,
+    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
+  ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
