e3762ec4f941ad193e78edea2ee2e7cbb3738aa1
Andrew Che <andrew@codeninja55.me>
2018-04-22 17:29:17 +1000

    Added link to new event form and closes both sidenav

--- frontend-angular/src/app/mat-fab/mat-fab.component.ts
+++ frontend-angular/src/app/mat-fab/mat-fab.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-mat-fab',
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mat-fab.component.css']
 })
 export class MatFabComponent implements OnInit {
+  @Output() navToggle = new EventEmitter<boolean>();
+  @Output() bookingsToggle = new EventEmitter<boolean>();
 
   constructor() { }
 
   ngOnInit() {
+    this.closeSidenav();
+    this.closeBookingsSidenav();
+  }
+
+  closeSidenav() {
+    this.navToggle.emit(true);
+  }
+  closeBookingsSidenav() {
+    this.bookingsToggle.emit(true);
   }
 
 }
