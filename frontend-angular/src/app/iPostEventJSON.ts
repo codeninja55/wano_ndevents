e77fcc9dbc254ebc77ca83e0b7ba008b0c473084
Andrew Che <andrew@codeninja55.me>
2018-04-23 16:39:10 +1000

    Created a specific interface to type check for JSON for api post endpt

---
+++ frontend-angular/src/app/iPostEventJSON.ts
@@ -0,0 +1,14 @@
+interface IPostEventJSON {
+  title: string;
+  venue: string;
+  capacity_max: number;
+  capacity_expected: number;
+  description: string;
+  organisers_name: number;
+  date_start: string;
+  date_end: string;
+  is_launched: boolean;
+  launch_date?: string;
+  time_start?: string;
+  time_end?: string;
+}
