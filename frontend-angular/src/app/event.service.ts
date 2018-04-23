a5c82e6efcf02fc52320018d9db03a1ee0a3fdcc
Andrew Che <andrew@codeninja55.me>
2018-04-23 16:42:24 +1000

    Cleaned up post event service

--- frontend-angular/src/app/event.service.ts
+++ frontend-angular/src/app/event.service.ts
@@ -35,7 +35,6 @@ export class EventService {
   // Uses http.post method to send JSON to the the create API endpoint to make a new event
   postEvent(data) {
     const body = JSON.stringify(data);
-    // return this._http.post(this._eventApi + 'create/', body, httpOptions);
     return this._http.post(this._eventApi + 'create/', body, httpOptions)
       .pipe(
         tap((newEvent) => console.log('[DEBUG]' + newEvent)),
