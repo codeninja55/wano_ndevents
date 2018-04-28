e26fa47df719c5bb700f1aa6a83610d7d974ad8a
Andrew Che <andrew@codeninja55.me>
2018-04-29 01:06:22 +1000

    ADded the FlexLayoutModule and EventDashboard Comp

--- frontend-angular/src/app/home/home.module.ts
+++ frontend-angular/src/app/home/home.module.ts
@@ -5,16 +5,22 @@ import {FormsModule} from '@angular/forms';
 import {DisplayCompService} from '../display-comp.service';
 import {HomeNavbarComponent} from './home-navbar/home-navbar.component';
 import {HomeComponent} from './home.component';
+import {HomeRoutingModule} from './home-routing.module';
+import {EventDashboardComponent} from '../event-dashboard/event-dashboard.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
+  imports: [
+    MaterialModule,
+    CommonModule,
+    FormsModule,
+    HomeRoutingModule,
+    FlexLayoutModule,
+  ],
   declarations: [
     HomeComponent,
     HomeNavbarComponent,
-  ],
-  imports: [
-    CommonModule,
-    FormsModule,
-    MaterialModule,
+    EventDashboardComponent,
   ],
   exports: [],
   providers: [ DisplayCompService, ]
