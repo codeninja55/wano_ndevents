500a07aaaad3d5f888c10c75f939dbf1a8cfedc2
Andrew Che <andrew@codeninja55.me>
2018-04-28 17:21:54 +1000

    Removed route debugging

--- frontend-angular/src/app/app-routing.module.ts
+++ frontend-angular/src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const appRoutes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(appRoutes,
-      { enableTracing: true}  // <-- For debugging purposes
+      // { enableTracing: true}  // <-- For debugging purposes
       ),
   ],
   exports: [
